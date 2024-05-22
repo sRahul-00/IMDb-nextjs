c4ef8bfeeb088d084943ce087ab5d451f4c3202e
Rahul <singhrahul1300@gmail.com>
2024-05-22 14:23:57 +0530

    changed loading page

--- src/app/loading.jsx
+++ src/app/loading.jsx
@@ -1,8 +1,10 @@
+import Image from "next/image"
+import spinner from "../../public/spinner.svg";
 
 const loading = () => {
   return (
     <div className="flex justify-center mt-14">
-        <img src="spinner.svg" alt="loading" />
+      <Image width={'450'} height={'450'} className="h-52" src={spinner} alt="loading..." />
     </div>
   )
 }
