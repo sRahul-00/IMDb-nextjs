cf6738382c03daced5434fe514740c543d86690b
Rahul <singhrahul1300@gmail.com>
2024-05-22 13:35:35 +0530

    changed name

--- src/app/loading.jsx
+++ src/app/loading.jsx
@@ -1,9 +1,9 @@
 
-const Loading = () => {
+const loading = () => {
   return (
     <div className="flex justify-center mt-14">
         <img src="spinner.svg" alt="loading" />
     </div>
   )
 }
-export default Loading
+export default loading
