4726d2cd12daa290895344fc0e3efc4984257d6d
Rahul <singhrahul1300@gmail.com>
2024-05-22 13:46:01 +0530

    added suspense for useSearchParams

--- src/components/NavbarItem.jsx
+++ src/components/NavbarItem.jsx
@@ -1,4 +1,5 @@
 "use client";
+import {Suspense} from "react";
 import Link from "next/link"
 import { useSearchParams } from "next/navigation"
 
@@ -6,9 +7,11 @@ const NavbarItem = ({title, param}) => {
     const searchParams = useSearchParams();
     const genre = searchParams.get('genre');
   return (
-    <Link href={`/?genre=${param}`} className={`hover:text-amber-700 font-semibold ${genre === param ? 'underline underline-offset-8 decoration-4 decoration-amber-600 rounded-lg' :''}`}>
-        {title}
-    </Link>
+    <Suspense>
+      <Link href={`/?genre=${param}`} className={`hover:text-amber-700 font-semibold ${genre === param ? 'underline underline-offset-8 decoration-4 decoration-amber-600 rounded-lg' :''}`}>
+          {title}
+      </Link>
+    </Suspense>
   )
 }
 export default NavbarItem
