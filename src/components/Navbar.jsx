dd1db2b04e4bf434b80223aacca2867b0048f627
Rahul <singhrahul1300@gmail.com>
2024-05-22 14:04:15 +0530

    add suspense

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,11 +1,14 @@
+import { Suspense } from "react"
 import NavbarItem from "./NavbarItem"
 
 const Navbar = () => {
   return (
-    <div className="flex lg:text-lg dark:bg-gray-600 bg-amber-100 p-4 font-medium gap-8 justify-center ">
-        <NavbarItem title="Trending" param="fetchTrending" />
-        <NavbarItem title="Top Rated" param="fetchTopRated" />
-    </div>
+    <Suspense>
+      <div className="flex lg:text-lg dark:bg-gray-600 bg-amber-100 p-4 font-medium gap-8 justify-center ">
+          <NavbarItem title="Trending" param="fetchTrending" />
+          <NavbarItem title="Top Rated" param="fetchTopRated" />
+      </div>
+    </Suspense>
   )
 }
 export default Navbar
--- src/components/NavbarItem.jsx
+++ src/components/NavbarItem.jsx
@@ -7,11 +7,9 @@ const NavbarItem = ({title, param}) => {
     const searchParams = useSearchParams();
     const genre = searchParams.get('genre');
   return (
-    <Suspense>
-      <Link href={`/?genre=${param}`} className={`hover:text-amber-700 font-semibold ${genre === param ? 'underline underline-offset-8 decoration-4 decoration-amber-600 rounded-lg' :''}`}>
-          {title}
-      </Link>
-    </Suspense>
+    <Link href={`/?genre=${param}`} className={`hover:text-amber-700 font-semibold ${genre === param ? 'underline underline-offset-8 decoration-4 decoration-amber-600 rounded-lg' :''}`}>
+        {title}
+    </Link>
   )
 }
 export default NavbarItem
